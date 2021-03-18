d20927791d54b1b0716d7b2fc98a93fb391db2d9
Region60 <_max_kot@mail.ru>
2021-03-19 00:18:51 +0300

    add_keys

---
+++ keys/keys.dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://maksim:8u2upvDe0W1dp945@cluster0-mjkka.mongodb.net/backEndGallery',
+    JWT_SECRET: 'some secret value',
+    BASE_URI: 'https://agile-fortress-83007.herokuapp.com/'
+}
